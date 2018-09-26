316993bc6ac067e752fb3dd5ed35b1d2ddbff260
Veronica Bustabad <vbustabad@gmail.com>
2018-09-26 19:59:57 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,3 +1,8 @@
 function hideWhenClicked() {
-
+  var x = document.getElementById("hide_this");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
 }
